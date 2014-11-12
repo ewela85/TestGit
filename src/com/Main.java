2ae23c75b5e1f9f245deda98b7f620250ef63ebd
Ewela <testeee.1980@o2.pl>
2014-11-12 19:25:12 +0100

    pierwszy

---
+++ src/com/Main.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * Created by Ewela on 2014-11-12.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("test gitHub1");
+    }
+}
