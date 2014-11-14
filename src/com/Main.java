f2bebb6e918338fac2ef1de40546a4df4e8b81ce
Ewela <testeee.1980@o2.pl>
2014-11-14 18:46:56 +0100

    6

--- src/com/Main.java
+++ src/com/Main.java
@@ -7,8 +7,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("branch merge do local master11");
-        System.out.println("branch merge do local master22");
+        System.out.println("branch merge do local master111");
+        System.out.println("branch merge do local master222");
         System.out.println("branch merge do local master3");
     }
 }
