ed97ac183e35090a9423310e03399370106c4bd9
Ewela <testeee.1980@o2.pl>
2014-11-17 18:41:03 +0100

    komit 2

--- src/com/Main.java
+++ src/com/Main.java
@@ -11,5 +11,6 @@ public class Main
         System.out.println("branch merge do local master2222");
         System.out.println("branch merge do local master3");
         System.out.println("komit1");
+        System.out.println("komit2");
     }
 }
