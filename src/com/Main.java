98cdbc8aabbf6e1dd4e7b7df889024165eaeea14
Ewela <testeee.1980@o2.pl>
2014-11-17 18:41:03 +0100

    komit

--- src/com/Main.java
+++ src/com/Main.java
@@ -8,6 +8,7 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("branch merge do local master");
+        System.out.println("master zmiany");
         System.out.println("branch merge do local master22223");
         System.out.println("branch merge do local master3");
         System.out.println("komit1");
